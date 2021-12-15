e2fd0fb6050e85549cfc6d8b0c85766e37ec7c03
Fabio Dela Bruna <fabio.bruna@agrotis.com>
2021-12-15 18:14:54 -0300

    Adicionando e configurando MirageJS

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        { id: 1, title: 'Desenvolvimento de WebSite', amount: 1000, type: 'deposit', category: 'Desenvolvimento', createAt: new Date()}
+      ]
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
